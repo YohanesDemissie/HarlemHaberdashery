ffaef1205e071ec0d599b2eeb8f4e36fa82fb854
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-03-31 16:56:18 -0700

    added completley functional timer and seperate vairables for time stamps to utilize for handling click, next

--- VideoPage.js
+++ VideoPage.js
@@ -1,64 +1,72 @@
 import React from 'react';
 import Video from 'react-native-video';
 import { StyleSheet } from 'react-native';
-import { View } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import Home from './app/pages/Home';
+import Story from './app/pages/Story';
+import Shop from './app/pages/Shop'
 
-let enterDate = new Date();
-const secondsSinceEnter = () => {
-  return (new Date() - enterDate) / 1000;
-}
 
-const handleClick = () => {
-  let sec = secondsSinceEnter();
-  while(sec < 5) {
-    sec++
-    console.log(sec, ': seconds, less than 5')
+let seconds = 0;
+//ADD CONSOLE.LOG('ADDING BY 1') TO END OF 'SECONDS += 1' TO READ ACCUMILATING DATA IN CONSOLE
+let timer = setInterval(() => {
+  seconds += 1;
+  if(seconds <= 5){
+    console.log(seconds, '0 - 5');
+    // return <Home />
   }
-    while(sec > 5 && sec < 10){
-      sec++
-      console.log(sec, 'more than 5 seconds')
-    }
+  if(seconds > 5 && seconds <= 10){
+        console.log(seconds, '5 - 10')
+      // return <Story />
   }
-// Within your render function, assuming you have a file called
-// "background.mp4" in your project. You can include multiple videos
-// on a single screen if you like.
+  if(seconds > 10 && seconds < 15){
+    console.log(seconds, '10 - 15')
+    // return <Shop />
+  }
+  if(seconds >= 15){
+    seconds = 0;
+    seconds += 1;
+  }
+},
+  1000
+);
 
-// let counter = 0;
-// const count = () => {
-//   window.onload = () => {
-//   setInterval(count, 1000)
-//   console.log(count, 'count')
-// };
-
-//   // while(0 <= counter && counter <= 5) {
-//   //   console.log('0 - 5 seconds', counter);
-//   // }
-//   // switch(counter){
-//   //   case (0 <= counter && counter <= 5):
-//   //     console.log('0 - 5 seconds');
-//   //     break;
-
-//   //     case 5 < counter < 10:
-//   //       console.log('5 - 10 seconds')
-//   //       break;
-
-//   //       case 10 < counter < 15:
-//   //         console.log('10 - 15 seconds')
-//   //         break;
-
-//   //         default: counter = 0;
-//   // }
+//handle click to coordinate right timing with right item to checkout
+// function handleClick() {
+//   if(timer <= 5){
+//       console.log(timer, 'BELOW 5');
+//     // return <Home />
+//   }
+//   if(timer > 5 && timer <= 10){
+//         console.log(timer, '5 - 10')
+//       // return <Story />
+//   }
+//   if(timer > 10 && timer < 15){
+//     console.log(timer, '10 - 15')
+//     // return <Shop />
+//   }
+  // if(timer >= 15){
+  //   timer = 0;
+  //   timer += 1;
+  // }
+  // return timer
 // }
+
+// BELOW THE SECONDS VARIABLE HOLDS THE VALUE OF TIME!!!!!!!!!!!!!!!!!!!!!!!
+
 const VideoPage = () => {
   return(
     <View style={{flex: 1}} >
-      {handleClick()}
+      <Text>
+        {timer}
+        {/* {handleClick()} */}
+      </Text>
+      {/* <Button onPress={handleClick()} title={'Timer'}>Timer</Button> */}
       <Video source={{uri: "https://www.w3schools.com/html/mov_bbb.mp4"}}
       style={{flex: 1}} />
     </View>
   )
 }
-
 
 // Later on in your styles..
 var styles = StyleSheet.create({
