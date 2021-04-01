6d03148933e26acb1a92a46746fc400ab1a15e64
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-04-01 14:03:48 -0700

    onPress rendering and returning data in reference to timer. Adding navigational functionality to timer value, next

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import VideoPage from './VideoPage';
-// import { Video } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import RootNavigation from './app/navigation/rootNavigation'
 import { navigationRef } from './app/navigation/navigationService'
 
 const App = () => (
+    // <NavigationContainer ref={navigationRef}>
+    //   <RootNavigation />
+    // </NavigationContainer>
   <VideoPage />
   );
 
--- VideoPage.js
+++ VideoPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Video from 'react-native-video';
 import { StyleSheet } from 'react-native';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableHighlight } from 'react-native';
+import Menu from './app/components/Menu';
 import Home from './app/pages/Home';
 import Story from './app/pages/Story';
 import Shop from './app/pages/Shop'
@@ -10,18 +11,19 @@ import Shop from './app/pages/Shop'
 let seconds = 0;
 //ADD CONSOLE.LOG('ADDING BY 1') TO END OF 'SECONDS += 1' TO READ ACCUMILATING DATA IN CONSOLE
 let timer = setInterval(() => {
+  let target;
   seconds += 1;
   if(seconds <= 5){
     console.log(seconds, '0 - 5');
-    // return <Home />
+    // return ( <Home /> )
   }
   if(seconds > 5 && seconds <= 10){
         console.log(seconds, '5 - 10')
-      // return <Story />
+      // return (<Story />)
   }
   if(seconds > 10 && seconds < 15){
     console.log(seconds, '10 - 15')
-    // return <Shop />
+    // return (<Shop /> )
   }
   if(seconds >= 15){
     seconds = 0;
@@ -31,37 +33,32 @@ let timer = setInterval(() => {
   1000
 );
 
-//handle click to coordinate right timing with right item to checkout
-// function handleClick() {
-//   if(timer <= 5){
-//       console.log(timer, 'BELOW 5');
-//     // return <Home />
-//   }
-//   if(timer > 5 && timer <= 10){
-//         console.log(timer, '5 - 10')
-//       // return <Story />
-//   }
-//   if(timer > 10 && timer < 15){
-//     console.log(timer, '10 - 15')
-//     // return <Shop />
-//   }
-  // if(timer >= 15){
-  //   timer = 0;
-  //   timer += 1;
-  // }
-  // return timer
-// }
-
 // BELOW THE SECONDS VARIABLE HOLDS THE VALUE OF TIME!!!!!!!!!!!!!!!!!!!!!!!
+function handleClick() {
+     if(seconds <= 5){
+      console.log(seconds, 'BELOW 5');
+    return (<Home />)
+  }
+  if(seconds > 5 && seconds <= 10){
+        console.log(seconds, '5 - 10')
+      return <Story />
+  }
+  if(seconds > 10 && seconds < 15){
+    console.log(seconds, '10 - 15')
+    return <Shop />
+  }
+}
 
-const VideoPage = () => {
+const VideoPage = ({ navigation }) => {
   return(
-    <View style={{flex: 1}} >
-      <Text>
-        {timer}
-        {/* {handleClick()} */}
-      </Text>
-      {/* <Button onPress={handleClick()} title={'Timer'}>Timer</Button> */}
+    <View style={{flex:1, flexDirection: 'column', justifyContent: 'center', padding: 20, marginTop: 50}} >
+      <Menu navigation={navigation} style={{textAlign: 'center'}}/>
+
+      <Button
+      style={{marginTop: 50}}
+        title="Press me"
+        onPress={() => handleClick()}
+      />
       <Video source={{uri: "https://www.w3schools.com/html/mov_bbb.mp4"}}
       style={{flex: 1}} />
     </View>
@@ -69,7 +66,7 @@ const VideoPage = () => {
 }
 
 // Later on in your styles..
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   backgroundVideo: {
     display: 'flex',
     flex: 1,
