90a6480cea1165e1851e17f7b7b8d5dab06c04f2
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-04-14 13:08:20 -0700

    videos rendering in full screen. touchable opacity redirecting videos to different components.

--- app/pages/VideoPage.js
+++ app/pages/VideoPage.js
@@ -42,77 +42,105 @@ const videoData = [
 let page;
 function handleClick(page) {
      if(seconds <= 5){
-       console.log(seconds, 'BELOW 5');
+      //  console.log(seconds, 'BELOW 5');
       page = ROUTES.HOME
   }
   if(seconds > 5 && seconds <= 10){
-        console.log(seconds, '5 - 10')
+        // console.log(seconds, '5 - 10')
       page = ROUTES.SHOP
   }
   if(seconds > 10 && seconds < 15){
-    console.log(seconds, '10 - 15')
+    // console.log(seconds, '10 - 15')
     page = ROUTES.STORY
   }
   return page;
 }
 
-const Blink = () => {
+// const VideoCarousel = () => {
+//   const [index, setIndex] = useState(0);
+
+//    useEffect(() => {
+//      const toggle = setInterval(() => {
+//        setIndex( index + 1)
+//      }, 10000);
+//      //MAKE VIDEO ARRAY LOOP BACK TO BEGINNING
+//      if(index > 2){
+//        setIndex(0)
+//      }
+//      return () => clearInterval(toggle);
+//   })
+
+//   return <Video source={videoData[index]}  style={{
+//         position: 'absolute',
+//         top: 150,
+//         left: 0,
+//         bottom: 0,
+//         right: 0,}}/>;
+// }
+
+const VideoPage = ({navigation}) => {
   const [index, setIndex] = useState(0);
 
    useEffect(() => {
      const toggle = setInterval(() => {
        setIndex( index + 1)
-     }, 5000);
-
+     }, 10000);
+     //MAKE VIDEO ARRAY LOOP BACK TO BEGINNING
+     if(index > 2){
+       setIndex(0)
+     }
      return () => clearInterval(toggle);
   })
-
-  //LETTERS IS THE PLACE HOLDER FOR VIDEODATA ARRAY
-  let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g'];
-  if(index > 2){
-    setIndex(0);
-  }
-  return <Video source={videoData[index]} style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,}}/>;
-}
-
-// const BlinkApp = () => {
-//   return (
-//     <View style={{marginTop: 50}}>
-//       <Blink />
-//     </View>
-//   );
-// }
-
-// export default BlinkApp;
-
-const VideoPage = ({navigation}) => {
   return(
-    <View style={{display: 'flex', flex: 1, flexDirection: 'column', marginTop: 100}}>
+    <View style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
       <Menu navigation={navigation} />
-      <Blink />
+      <TouchableOpacity style={{display: 'flex', flex: 1}} onPress={() => {
+        if(seconds <= 5){
+          // console.log(seconds, 'BELOW 5');
+          navigation.navigate(ROUTES.HOME)
+        }
+        if(seconds > 5 && seconds <= 10){
+          // console.log(seconds, '5 - 10')
+          navigation.navigate(ROUTES.SHOP)
+        }
+        if(seconds > 10 && seconds < 15){
+          // console.log(seconds, '10 - 15')
+          navigation.navigate(ROUTES.STORY)}
+      }}>
 
-    <Button title="Test" onPress={() => {
-      if(seconds <= 5){
-        console.log(seconds, 'BELOW 5');
-        navigation.navigate(ROUTES.HOME)
-      }
-      if(seconds > 5 && seconds <= 10){
-        console.log(seconds, '5 - 10')
-        navigation.navigate(ROUTES.SHOP)
-      }
-      if(seconds > 10 && seconds < 15){
-        console.log(seconds, '10 - 15')
-        navigation.navigate(ROUTES.STORY)}
-      }}
-    />
+      <Video 
+        fullscreen={true}
+        resizeMode="cover"
+        key={index}
+        source={videoData[index]}
+        style={{
+          position: 'absolute',
+          viewHeight: '400%',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        }}/>
+
+      </TouchableOpacity>
+      {/* <Button title="Test" onPress={() => {
+        if(seconds <= 5){
+          console.log(seconds, 'BELOW 5');
+          navigation.navigate(ROUTES.HOME)
+        }
+        if(seconds > 5 && seconds <= 10){
+          console.log(seconds, '5 - 10')
+          navigation.navigate(ROUTES.SHOP)
+        }
+        if(seconds > 10 && seconds < 15){
+          console.log(seconds, '10 - 15')
+          navigation.navigate(ROUTES.STORY)}
+        }}
+      /> */}
     </View>
   )
 }
+
 
 // Later on in your styles..
 const styles = StyleSheet.create({
