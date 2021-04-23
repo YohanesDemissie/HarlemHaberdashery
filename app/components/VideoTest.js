35db88da24a3189622a58c4df3d6e8ba790653ad
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-04-23 15:45:06 -0700

    added edited videos for final video slide show. need some final editing ofr transitions and fluency.

--- app/components/VideoTest.js
+++ app/components/VideoTest.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Video from 'react-native-video';
-import { View, Text, Button, TouchableOpacity, StyleSheet, Linking } from 'react-native';
+import { View, ImageBackground, Text, Button, TouchableOpacity, StyleSheet, Linking } from 'react-native';
+import HHLogo from '../../assets/harlemHaberdasheryLogo.png'
 import Menu from '../components/Menu';
 import HHJacketPt1 from '../../assets/videos/HHJacketPt1.mp4';
+import HHLogoPatchVideo from '../../assets/videos/HHLogoPatchVideo.mp4';
 import HHWaterPt2 from '../../assets/videos/HHWaterPt2.mp4';
 import HHSpiritsPt3 from '../../assets/videos/HHSpiritsPt3.mp4';
-
+import HHJacketVideo from '../../assets/videos/HHJacketVideo.mp4';
+import HHWaterVideo from '../../assets/videos/HHWaterVideo.mp4';
+import HHSpiritsVideo from '../../assets/videos/HHSpiritsVideo.mp4';
 class VideoTest extends Component {
   constructor(props) {
     super(props);
@@ -27,12 +31,12 @@ forceUpdateHandler(){
 
 timer = setInterval(() => {
   this.setState({seconds: this.state.seconds += 1});
-  if(this.state.index === 3){
+  if(this.state.index === 4){
     this.setState({index: 0});
     this.setState({ seconds: 0});
     console.log(this.state.seconds, 'this.state.seconds')
   }
-  if(this.state.seconds >= 10){
+  if(this.state.seconds > 7){
     this.setState({index: this.state.index += 1})
     this.setState({seconds: 0});
     // this.state.index = this.state.index + 1;
@@ -46,43 +50,52 @@ timer = setInterval(() => {
 
 render() {
   const videoData = [
-    HHJacketPt1,
-    HHWaterPt2,
-    HHSpiritsPt3,
+    HHJacketVideo,
+    HHLogoPatchVideo,
+    HHWaterVideo,
+    HHSpiritsVideo
 ];
   return(
     <View style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
       {/* <Menu navigation={navigation} /> */}
-      <TouchableOpacity style={{display: 'flex', flex: 1}} onPress={() => {
-        if(this.state.index === 0){
-          console.log(this.state.seconds, 'PURCHASING JACKET');
-          Linking.openURL('https://www.harlemhaberdashery.com/')
-        } else
-        if(this.state.index === 1){
-          console.log(this.state.seconds, 'PURCHASING WATER')
-          Linking.openURL('https://www.marvelouswaters.com/buy')
-        } else
-        if(this.state.index >= 2){
-          console.log(this.state.seconds, 'PURCHASING LIQUOR')
-          Linking.openURL('https://hhbespokespirits.com/buy-now')
-        }
-      }}>
+      <ImageBackground source={HHLogo} style={{flex: 1, height: '20%', justifyContent: 'center'}}>
+        <TouchableOpacity style={{display: 'flex', flex: 1}} onPress={() => {
+          if(this.state.index === 0){
+            console.log(this.state.seconds, 'PURCHASING JACKET');
+            Linking.openURL('https://www.harlemhaberdashery.com/')
+          } else
+          if(this.state.index === 1){
+            console.log(this.state.seconds, 'PURCHASING WATER')
+            Linking.openURL('https://www.marvelouswaters.com/buy')
+          } else
+          if(this.state.index >= 2){
+            console.log(this.state.seconds, 'PURCHASING LIQUOR')
+            Linking.openURL('https://hhbespokespirits.com/buy-now')
+          }
+          if(this.state.index >= 3){
+            console.log(this.state.seconds, 'PURCHASING LIQUOR')
+            Linking.openURL('https://hhbespokespirits.com/buy-now')
+          }
+        }}>
 
-      <Video
-        // fullscreen={true}
-        // resizeMode="cover"
-        source={videoData[this.state.index]}
-        seekTime={this.state.seconds}
-        muted={true}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0
-        }}/>
+    
+        <Video
+          // fullscreen={true}
+          // resizeMode="cover"
+          source={videoData[this.state.index]}
+          seekTime={this.state.seconds}
+          muted={true}
+          style={{
+            position: 'absolute',
+            width: '100%',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0
+          }}/>
 
-      </TouchableOpacity>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   )
 }
