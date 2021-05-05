9a3986d42dd23008f07b4cd09c619774a632fa00
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-05-05 13:38:49 -0700

    reduced video sizes by 09% for optimal load time.

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -7,9 +7,9 @@ export default ({ navigation }) => {
     <View style={styles.wrapper}>
       <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
         <Text style={{color: 'white', marginLeft: 8, fontSize: 18}}>Menu</Text>
-        <View style={{borderTopColor: 'white', borderRadius: 3, marginLeft: 8, marginBottom: 5, borderWidth: 1, width: 47, height: 0, top: 0}} />
-        <View style={{borderTopColor: 'white', borderRadius: 3, marginLeft: 8, marginBottom: 5, borderWidth: 1, width: 47, height: 0, top: 0, bottom: 0}} />
-        <View style={{borderTopColor: 'white', borderRadius: 3, marginLeft: 8, marginBottom: 5, borderWidth: 1, width: 47, height: 0, top: 0}} />
+        <View style={{borderTopColor: 'white', marginLeft: 8, marginBottom: 5, borderWidth: 1, width: 47, height: 0, top: 0}} />
+        <View style={{borderTopColor: 'white', marginLeft: 8, marginBottom: 5, borderWidth: 1, width: 47, height: 0, top: 0, bottom: 0}} />
+        <View style={{borderTopColor: 'white', marginLeft: 8, marginBottom: 5, borderWidth: 1, width: 47, height: 0, top: 0}} />
         <View style={{borderTopColor: 'black', marginLeft: 8, borderWidth: 1, width: 47, height: 0, top: 0}} />
       </TouchableOpacity>
     </View>
@@ -24,7 +24,6 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     paddingTop: '10%',
     backgroundColor: 'black',
-    
     // borderBottomColor: 'white',
     // borderWidth: 1,
     // width: 100
